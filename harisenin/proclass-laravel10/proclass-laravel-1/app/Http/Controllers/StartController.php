ec9fc7c13927e1bdabfb50b2792daf007b26131b
bayufadayan <bayufadayan@gmail.com>
2026-01-06 23:10:18 +0700

    chore: add all changes

---
+++ harisenin/proclass-laravel10/proclass-laravel-1/app/Http/Controllers/StartController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StartController extends Controller
+{
+    public function index() {
+        return view("hello");
+    }
+
+    public function helloView($name) {
+        return view("hello-name", ['name' => $name]);
+    }
+}
